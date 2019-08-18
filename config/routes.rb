2228f2d32428ae912621debb745ec9f0c0c4503f
gouravthakur39 <gourav.thakur39@gmail.com>
2019-08-18 21:49:27 +0530

    Add jumbotron on home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  root to: 'pages#home'
   resources :articles
+  get "/pages/about", :controller => "pages", :action => "show", :id => "about"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
