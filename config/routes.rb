dcbaa4c611927f05d797380a60c1441925a52836
gouravthakur39 <gourav.thakur39@gmail.com>
2019-08-26 21:29:11 +0530

    Sign up user from UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :articles
   get "/pages/about", :controller => "pages", :action => "show", :id => "about"
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create' or it can be done like this
+  resources :users, except: [:new]
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
