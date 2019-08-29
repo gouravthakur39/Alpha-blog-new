2fad378964be5419afe3da407a44407fde3572cc
gouravthakur39 <gourav.thakur39@gmail.com>
2019-08-29 16:19:35 +0530

    restrict action in controller and authentication system complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit,:update, :show, :destroy]
-		def index
-			@articles= Article.paginate(page: params[:page],per_page: 5)
-		end
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+
+	def index
+		@articles= Article.paginate(page: params[:page],per_page: 5)
+	end
 
     def new
         @article = Article.new
@@ -22,23 +25,23 @@ class ArticlesController < ApplicationController
         end
 		end
 		
-		def update
-			if @article.update(article_params)
-				flash[:success] = "Article was successfully updated"
-				redirect_to article_path(@article)
-			else
-				render 'edit'
-			end
+	def update
+		if @article.update(article_params)
+			flash[:success] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
 		end
+	end
 
-		def show	
-		end
+	def show	
+	end
 
-		def destroy
-			@article.destroy
-			flash[:danger] = "Article was successfully deleted"
-			redirect_to articles_path 
-		end
+	def destroy
+		@article.destroy
+		flash[:danger] = "Article was successfully deleted"
+		redirect_to articles_path 
+	end
 
 	private
 	  def set_article
@@ -47,6 +50,13 @@ class ArticlesController < ApplicationController
 
       def article_params
         params.require(:article).permit(:title,:description)
-      end
+	  end
+	  
+	  def require_same_user
+		if current_user != @article.user
+			flash[:danger] = "You can only edit or delete your own articles"
+			redirect_to root_path
+		end
+	  end
 
 end
