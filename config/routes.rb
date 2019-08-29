6d5b0e2afce7c84c259ab1bfbfd05fb444519b27
gouravthakur39 <gourav.thakur39@gmail.com>
2019-08-29 14:03:30 +0530

    add login form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   #post 'users', to: 'users#create' or it can be done like this
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
